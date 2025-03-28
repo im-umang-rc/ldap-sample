a317ee63feea10c87c2937a1a7195674de696160
Umang Chaudhari <im.umang.rc@gmail.com>
2025-03-28 00:09:08 -0500

    Improving performance by optimizing the number of calls made to LDAP

--- src/main/java/portfolio/umang/service/LdapService.java
+++ src/main/java/portfolio/umang/service/LdapService.java
@@ -5,10 +5,7 @@ import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.*;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class LdapService {
 
@@ -27,35 +24,39 @@ public class LdapService {
         return Arrays.asList(connectionProperties.getProperty("servers").split(","));
     }
 
+    public static String getProperty(String key) {
+        return connectionProperties.getProperty(key);
+    }
 
-    public static boolean isUserLocked(String target, String uid) throws NamingException {
+    public static Map<String, Boolean> getUserStatus(String target, String searchString) throws NamingException {
         Hashtable<String, String> environment = new Hashtable<>();
         environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        environment.put(Context.PROVIDER_URL, connectionProperties.getProperty(target + ".url"));
+        environment.put(Context.PROVIDER_URL, getProperty(target + ".url"));
         environment.put(Context.SECURITY_AUTHENTICATION, "simple");
-        environment.put(Context.SECURITY_PRINCIPAL, connectionProperties.getProperty(target + ".user"));
-        environment.put(Context.SECURITY_CREDENTIALS, connectionProperties.getProperty(target + ".pwd"));
+        environment.put(Context.SECURITY_PRINCIPAL, getProperty(target + ".user"));
+        environment.put(Context.SECURITY_CREDENTIALS, getProperty(target + ".pwd"));
         DirContext context = null;
+        Map<String, Boolean> userStatus = new HashMap<>();
         try {
             context = new InitialDirContext(environment);
 
-            String filter = "(&(objectClass=" + connectionProperties.getProperty(target + ".obj") + ")" +
-                    "(" + connectionProperties.getProperty(target + ".id") + "=" + uid + "))";
-            String[] attrIDs = {"pwdAccountLockedTime"};
+            String[] attrIDs = {getProperty(target + ".id"), getProperty(target + ".lockAttr")};
             SearchControls searchControls = new SearchControls();
             searchControls.setReturningAttributes(attrIDs);
             searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 
             NamingEnumeration<SearchResult> searchResults
-                    = context.search(connectionProperties.getProperty(target + ".base"), filter, searchControls);
+                    = context.search(getProperty(target + ".base"), searchString, searchControls);
 
-            if (searchResults.hasMore()) {
+            while (searchResults.hasMore()) {
                 SearchResult result = (SearchResult) searchResults.next();
                 Attributes attrs = result.getAttributes();
-                return attrs.get("pwdAccountLockedTime") != null;
-            } else return false;
+                userStatus.put(attrs.get(getProperty(target + ".id")).get().toString(), attrs.get(getProperty(target + ".lockAttr")) != null);
+            }
+            return userStatus;
         } finally {
             if (context != null) context.close();
+
         }
     }
 }
--- src/main/java/portfolio/umang/service/UserService.java
+++ src/main/java/portfolio/umang/service/UserService.java
@@ -4,19 +4,47 @@ import portfolio.umang.dto.UserDTO;
 
 import javax.naming.NamingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static portfolio.umang.service.LdapService.getProperty;
+import static portfolio.umang.service.LdapService.getUserStatus;
 
 public class UserService {
 
+    private static final String parentTemplate = "(&(objectClass=%s)(|%s))";
+    private static final String childTemplate = "(%s=%s)";
+
     public List<UserDTO> getUsers(List<String> users) throws NamingException {
-        List<UserDTO> usersAvailable = new ArrayList<>();
-        List<String> servers = LdapService.getTargets();
-        for (String user : users) {
-            boolean status = false;
-            for (String server: servers) {
-                status = status || LdapService.isUserLocked(server, user);
+        String[] servers = getProperty("servers").split(",");
+        Map<String, Boolean> overAllStatus = new HashMap<>();
+
+        for (String server : servers) {
+            StringBuilder childTemplateCollector = new StringBuilder();
+            String uidAttr = getProperty(server + ".id");
+
+            for (String user : users) {
+                childTemplateCollector.append(
+                        childTemplate.formatted(uidAttr, user)
+                );
             }
-            usersAvailable.add(new UserDTO(user, status));
+
+            String searchString = parentTemplate.formatted(
+                    getProperty(server + ".obj"),
+                    childTemplateCollector.toString()
+            );
+
+            Map<String, Boolean> userStatus = getUserStatus(server, searchString);
+
+            for (String user : userStatus.keySet()) {
+                overAllStatus.compute(user, (k, v) -> v == null ? userStatus.get(user) : userStatus.get(user) || v);
+            }
+        }
+
+        List<UserDTO> usersAvailable = new ArrayList<>();
+        for (String user : overAllStatus.keySet()) {
+            usersAvailable.add(new UserDTO(user, overAllStatus.get(user)));
         }
         return usersAvailable;
     }
